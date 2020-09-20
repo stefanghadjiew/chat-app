bb9fafcc0e0719dd6b5c623276570f66fab434b5
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-21 01:28:43 +0300

    deploying on heroku

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/chat-app",{
+mongoose.connect("mongodb+srv://cheffo0o:123smokeweed123@cluster0.qus4z.mongodb.net/chat-app?retryWrites=true&w=majority",{
         useNewUrlParser: true ,
         useUnifiedTopology: true , 
         useCreateIndex : true 
